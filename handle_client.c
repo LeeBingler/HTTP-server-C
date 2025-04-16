cad7af66d62fbd643aa8ff38269b807a8650d161
LeeBingler <binglerlee@gmail.com>
2025-04-16 19:00:28 +0200

    feat: parse headers of request

--- handle_client.c
+++ handle_client.c
@@ -6,40 +6,151 @@
 
 #include <sys/socket.h>
 
-#define BUFF_SIZE 1080
+#define BUFF_SIZE 1024
+
+typedef struct HttpHeader {
+    char *key;
+    char *value;
+    struct HttpHeader *next;
+} HttpHeader_t;
 
 typedef struct {
     char *method;
     char *path;
     char *protocol;
-    char *headers;
-    char *data;
-} header_t;
+    HttpHeader_t *headers;
+    int header_count;
+    char *datas;
+} request_t;
 
-header_t *parse_request(char *request) {
-    header_t *header = malloc(sizeof(header_t));
-    char *cp_request = malloc(sizeof(char) * (BUFF_SIZE + 1));
-    
-    if(strcpy(cp_request, request) == NULL) {
-        perror("strcpy() in parse_request");
-        free(header);
+void free_request(request_t *request) {
+    if (request == NULL) return;
+
+    if (request->method != NULL) free(request->method);
+    if (request->path != NULL) free(request->path);
+    if (request->protocol != NULL) free(request->protocol);
+    if (request->datas != NULL) free(request->datas);
+
+    if (request->headers != NULL) {
+        HttpHeader_t *ptr = request->headers;
+        while (ptr != NULL) {
+            ptr = ptr->next;
+            free(request->headers->key);
+            free(request->headers->value);
+            free(request->headers);
+            request->headers = ptr;
+        }
+    }
+
+    free(request);
+}
+
+int parse_fl(char *line, request_t *request) {
+    char **map = malloc(sizeof(char *) * 3);
+
+    if (!line || !request || !map) {
+        perror("First line request invalid");
+        return errno;
+    }
+
+    int counter = 0;
+    int start = 0;
+    int len = strlen(line);
+    for (int i = 0; i <= len; i++) {
+        if (line[i] == ' ' || line[i] == '\0') {
+            line[i] = '\0';
+            map[counter] = strndup(line + start, i - start);
+            start = i + 1;
+            counter++;
+        }
+    }
+
+    if (!map[0] || !map[1] || !map[2]) {
+        perror("First line request invalid");
+        free(request);
+        return errno;
+    }
+
+    request->method = map[0];
+    request->path = map[1];
+    request->protocol = map[2];
+    free(map);
+    return 0;
+}
+
+int parse_headers(request_t *request) {
+    char *line;
+
+    while ((line = strtok(NULL, "\n\r")) && strlen(line) > 0) {
+        HttpHeader_t *node = calloc(1, sizeof(HttpHeader_t));
+        char *two_point = strchr(line, ':');
+
+        if (two_point) {
+            *two_point = '\0';
+            char *key = line;
+            char *value = two_point + 1;
+            HttpHeader_t *ptr = request->headers;
+            while (*value == ' ') value++;
+
+            node->key = strdup(key);
+            node->value = strdup(value);
+
+            if (ptr == NULL) {
+                request->headers = node;
+                request->header_count++;
+            } else {
+                while (ptr->next != NULL)
+                ptr = ptr->next;
+
+                ptr->next = node;
+                request->header_count++;
+            }
+        } else {
+            break;
+        }
+    }
+
+    return 0;
+}
+
+request_t *parse_request(char *raw_request) {
+    request_t *request = calloc(1, sizeof(request_t));
+    char *line = strtok(raw_request, "\r\n");
+
+    if (request == NULL) {
+        perror("calloc() in parse_request");
+        return request;
+    }
+
+    // Parse first line
+    if (parse_fl(line, request)) {
+        free(request);
         return NULL;
     }
 
-    if (header == NULL) {
-        perror("calloc() in parse_request");
-        return header;
+    // Parse Header
+    if (parse_headers(request)) {
+        free(request);
+        return NULL;
     }
 
-    printf("cp_request:\n\n%s\n", cp_request);
+    if (0) {
+        printf("method : %s path : %s protocol : %s \n\n", request->method, request->path, request->protocol);
+        printf("headers : \n\n");
+        HttpHeader_t *ptr = request->headers;
+        while (ptr != NULL) {
+            printf("%s : %s\n", ptr->key, ptr->value);
+            ptr = ptr->next;
+        }
+    }
 
-    return header;
+    return request;
 }
 
 int handle_client(int client_fd) {
     char buff[BUFF_SIZE] = { 0 };
     int byte_recv = recv(client_fd, buff, BUFF_SIZE, MSG_DONTWAIT);
-    
+
     if (byte_recv == -1) {
         perror("recv()");
         close(client_fd);
@@ -51,15 +162,15 @@ int handle_client(int client_fd) {
         return 0;
     }
 
-    header_t *header = parse_request(buff);
-    if (header == NULL)
+    request_t *request = parse_request(buff);
+    if (request == NULL)
         return errno;
 
-    char mess_header[17] = "HTTP/1.1 200 OK\n"; // Content-type + Date + connection + transfer-encoding
-    send(client_fd, mess_header, 17, MSG_CONFIRM);
+    char *mess_header = "HTTP/1.1 200 OK\n"; // Content-type + Date + connection + transfer-encoding
+    send(client_fd, mess_header, strlen(mess_header), MSG_CONFIRM);
     printf("send\n");
 
     close(client_fd);
-    free(header);
+    free_request(request);
     return 0;
 }
