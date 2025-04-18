3a8afb777105b0b1c041ff75a31e0fb7daa7997b
LeeBingler <binglerlee@gmail.com>
2025-04-18 12:30:34 +0200

    structure of project

---
+++ server/include/http-headers.h
@@ -0,0 +1,9 @@
+#ifndef HTTP_HEADERS_H
+#define HTTP_HEADERS_H
+
+int send_date(int client_fd);
+int send_contentlength(int client_fd, FILE *file);
+int send_contenttype(int client_fd, char *filename);
+int send_connection(int client_fd);
+
+#endif
---
+++ server/include/mime_types.h
@@ -0,0 +1,39 @@
+#ifndef MIME_TYPES_H
+#define MIME_TYPES_H
+
+const char* get_mime_type(const char *ext) {
+    if (!ext) return "application/octet-stream";
+
+    // Texts
+    if (strcasecmp(ext, "html") == 0) return "text/html";
+    if (strcasecmp(ext, "htm")  == 0) return "text/html";
+    if (strcasecmp(ext, "txt")  == 0) return "text/plain";
+    if (strcasecmp(ext, "css")  == 0) return "text/css";
+    if (strcasecmp(ext, "csv")  == 0) return "text/csv";
+
+    // Images
+    if (strcasecmp(ext, "jpg")  == 0) return "image/jpeg";
+    if (strcasecmp(ext, "jpeg") == 0) return "image/jpeg";
+    if (strcasecmp(ext, "png")  == 0) return "image/png";
+    if (strcasecmp(ext, "gif")  == 0) return "image/gif";
+    if (strcasecmp(ext, "svg")  == 0) return "image/svg+xml";
+    if (strcasecmp(ext, "ico")  == 0) return "image/x-icon";
+
+    // Scripts
+    if (strcasecmp(ext, "js")   == 0) return "application/javascript";
+    if (strcasecmp(ext, "json") == 0) return "application/json";
+    if (strcasecmp(ext, "xml")  == 0) return "application/xml";
+
+    // Fonts
+    if (strcasecmp(ext, "woff") == 0) return "font/woff";
+    if (strcasecmp(ext, "woff2")== 0) return "font/woff2";
+    if (strcasecmp(ext, "ttf")  == 0) return "font/ttf";
+
+    // PDF / documents
+    if (strcasecmp(ext, "pdf")  == 0) return "application/pdf";
+    if (strcasecmp(ext, "zip")  == 0) return "application/zip";
+
+    return "application/octet-stream"; // default
+}
+
+#endif // MIME_TYPES_H
