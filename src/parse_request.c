7770ca8fe2c10d873332187381dc84f6c196b724
LeeBingler <binglerlee@gmail.com>
2025-04-19 20:02:10 +0200

    fix: clean parse_request

--- src/parse_request.c
+++ src/parse_request.c
@@ -5,8 +5,10 @@
 
 #include "../include/parse_request.h"
 
+#define ERR_EXIT(msg) (perror(msg), errno)
+
 void free_request(request_t *request) {
-    if (request == NULL) return;
+    if (!request) return;
 
     if (request->method != NULL) free(request->method);
     if (request->path != NULL) free(request->path);
@@ -15,7 +17,7 @@ void free_request(request_t *request) {
 
     if (request->headers != NULL) {
         HttpHeader_t *ptr = request->headers;
-        while (ptr != NULL) {
+        while (ptr) {
             ptr = ptr->next;
             free(request->headers->key);
             free(request->headers->value);
@@ -30,28 +32,19 @@ void free_request(request_t *request) {
 int parse_fl(char *line, request_t *request) {
     char **map = malloc(sizeof(char *) * 3);
 
-    if (!line || !request || !map) {
-        perror("First line request invalid");
-        return errno;
-    }
+    if (!line || !request || !map) return ERR_EXIT("First line request invalid");
 
     // Put each word separated by a space in map
-    int counter = 0;
-    int start = 0;
-    int len = strlen(line);
+    int counter = 0, start = 0, len = strlen(line);
     for (int i = 0; i <= len; i++) {
         if (line[i] == ' ' || line[i] == '\0') {
             line[i] = '\0';
-            map[counter] = strndup(line + start, i - start);
+            map[counter++] = strndup(line + start, i - start);
             start = i + 1;
-            counter++;
         }
     }
 
-    if (!map[0] || !map[1] || !map[2]) {
-        perror("First line request invalid");
-        return errno;
-    }
+    if (!map[0] || !map[1] || !map[2]) return ERR_EXIT("First line request invalid");
 
     request->method = map[0];
     request->path = map[1];
@@ -64,43 +57,37 @@ int parse_headers(request_t *request) {
     char *line;
 
     while ((line = strtok(NULL, "\n")) && strlen(line) > 2) {
-        HttpHeader_t *node = calloc(1, sizeof(HttpHeader_t));
-        char *two_point = strchr(line, ':');
+        HttpHeader_t *node;
+        char *colon = strchr(line, ':');
 
-        if (!two_point || !node)
-            return 1;
+        if (!colon) return 1;
 
-        *two_point = '\0';
+        *colon = '\0';
         char *key = line;
-        char *value = two_point + 1;
-        HttpHeader_t *ptr = request->headers;
+        char *value = colon + 1;
         while (*value == ' ') value++;
 
+        node = calloc(1, sizeof(HttpHeader_t));
+        if (!node) return 1;
+
         node->key = strdup(key);
-
-        if (!(node->key)) {
-            free(node);
-            return 1;
-        }
-
         node->value = strdup(value);
-
-        if (!(node->value)) {
+        if (!node->key || !node->value) {
             free(node->key);
+            free(node->value);
             free(node);
             return 1;
         }
 
-        if (ptr == NULL) {
+        if (!request->headers) {
             request->headers = node;
-            request->header_count++;
         } else {
-            while (ptr->next != NULL)
-                ptr = ptr->next;
-
+            HttpHeader_t *ptr = request->headers;
+            while (ptr->next) ptr = ptr->next;
             ptr->next = node;
-            request->header_count++;
         }
+
+        request->header_count++;
 
         if (strcmp(node->key, "Host") == 0) request->host = node;
     }
@@ -113,37 +100,22 @@ int parse_datas(request_t *request) {
      * Should use a parser like cJSON to make the request data cleaner
      */
     char *line = strtok(NULL, "\n\r");
-
-    if (!line)
-        return 0;
+    if (!line) return 0;
 
     request->datas = strdup(line);
-
-    if (!request->datas)
-        return 1;
-    return 0;
+    return request->datas ? 0 : 1;
 }
 
 request_t *parse_request(char *raw_request) {
     request_t *request = calloc(1, sizeof(request_t));
     char *line = strtok(raw_request, "\r\n");
 
-    if (request == NULL) {
-        perror("calloc() in parse_request");
-        return request;
-    }
-
-    if (parse_fl(line, request)) {
-        free_request(request);
+    if (!request) {
+        perror("calloc request");
         return NULL;
     }
 
-    if (parse_headers(request)) {
-        free_request(request);
-        return NULL;
-    }
-
-    if(parse_datas(request)) {
+    if (parse_fl(line, request) || parse_headers(request) || parse_datas(request)) {
         free_request(request);
         return NULL;
     }
